3d7c1ed96d10380657928da5f974faf1bb6b240c
Cwek <dyf123456789@gmail.com>
2015-03-03 14:14:17 +0800

    1

--- collapsibleDivs.js
+++ collapsibleDivs.js
@@ -20,7 +20,167 @@ cd.expandCaption = wgULS('显示', '顯示');
  
 cd.navigationBarHide = cd.collapseCaption + '▲';
 cd.navigationBarShow = cd.expandCaption + '▼';
- 
+
+cd.clazzName={
+    CD_Tag:"NavFrame",
+    /*
+    Status
+    */
+    collapsed:"collapsed",
+    //uncollapsed:"",
+    /*
+    Body
+    */
+    NC:"NavContent",
+    NC_TS:"toggleShow",
+    NC_TH:"toggleHide",
+    /*
+    Head
+    */
+    NH:"NavHead",
+    NH_T:"NavToggle",
+    NH_TS:"toggleShow",
+    NH_TH:"toggleHide",
+    /*
+    Button
+    */
+    CD_HS:"toggleHotspot"
+} 
+cd.init=function(){
+    var divIndex = 0;
+    var NavigationBoxes = new Array();
+    var Divs=$("div.",cd.clazzName.CD_Tag);
+    
+    Divs.each(function(){
+        var $div=$(this);
+        var head_match=$div.find("div."+cd.clazzName.NH).first();
+        var THS_match=$div.find("."+cd.clazzName.CD_HS).first();
+        if(!head_match|!THS_match){
+            return;
+        }
+        
+        var id=(divIndex++);
+        $div.attr("id",cd.clazzName.CD_Tag+id);
+        
+        var iscollapsed=false;
+        {
+            var t_=$div.find(".",cd.clazzName.collapsed);
+            iscollapsed=!t_?false:true;
+        }        
+        var customHead=false;
+        var customHotspot=false;
+        var customBody=false;
+        var $openspot,$closespot,$head;
+        var $showbody,$hidebody;
+        {//head,hotspot
+            var head=$div.find("."+cd.clazzName.NH).first();
+            var head_show=$div.find("."+cd.clazzName.NH+">."+cd.clazzName.NH_TS).first();
+            var head_hide=$div.find("."+cd.clazzName.NH+">."+cd.clazzName.NH_TH).first();
+            var t_customHS=$div.find("."+cd.clazzName.CD_HS).frist();
+            
+            if(!head){
+                customHead=false;
+                $head=head;
+            }else{
+                customHead=true;              
+            }
+            
+            if(!t_customHS&&(head_show&&head_hide)){
+                customHotspot=false;
+                $openspot=head_show;
+                $closespot=head_hide;
+            }else{
+                customHotspot=true;
+                $openspot=$closespot=t_customHS;
+            }
+        }
+        if(customHead&!customHotspot){        
+            var navToggle = $( '<span class="NavToggle"></span>' );
+            var navShow=$('<span class="toggleShow"></span>').text(cd.navigationBarShow);
+            var navHide=$('<span class="toggleHide"></span>').text(cd.navigationBarHide);
+            navToogle.append(navShow).append(navHide);
+            $head.append(navToogle);
+            $openspot=$closespot=$navToogle;
+        }
+        var clickhandle=cd.createClickHandlerBuilder(id);
+        $openspot.bind("click",clickhandle);
+        $closespot.bind("click",clickhandle);
+        $head.bind("click",clickhandle);        
+    });
+}
+
+
+cd.createClickHandlerBuilder=function(index){
+    return function(event){
+        var target = $(e.target);
+        var $div=$("div#"+cd.clazzName.CD_Tag+index)   ;
+    
+        var iscollapsed=false;
+        {
+            var t_=$div.find(".",cd.clazzName.collapsed);
+            iscollapsed=!t_?false:true;
+        }        
+        var customHead=false;
+        var customHotspot=false;        
+        var $openspot,$closespot,$head;
+        {//head,hotspot
+            var head=$div.find("."+cd.clazzName.NH).first();
+            var head_show=$div.find("."+cd.clazzName.NH+">."+cd.clazzName.NH_TS).first();
+            var head_hide=$div.find("."+cd.clazzName.NH+">."+cd.clazzName.NH_TH).first();
+            var t_customHS=$div.find("."+cd.clazzName.CD_HS).frist();
+            
+            if(!head){
+                customHead=false;
+                $head=head;
+            }else{
+                customHead=true;              
+            }
+            
+            if(!t_customHS&&(head_show&&head_hide)){
+                customHotspot=false;
+                $openspot=head_show;
+                $closespot=head_hide;
+            }else{
+                customHotspot=true;
+                $openspot=$closespot=t_customHS;
+            }
+        }       
+        
+        //cd.toggle(index);
+    }
+};
+
+cd.show=function($block){
+    $block.each(function(){$(this).css( 'display', 'block' );});
+}
+
+cd.hide=function($block){
+    $block.each(function(){$( this ).css( 'display', 'none' );});
+}
+
+cd.toggle=function(index,iscollapsed){
+    var $div=$("div#"+cd.clazzName.CD_Tag+index);    
+         
+    var customHead=false;
+    var customHotspot=false;
+    var customBody=false;
+    var $openspot,$closespot,$head;
+    var $showbody,$hidebody;    
+    {//body
+        var t_body=$div.find("."+cd.clazzName.NC).first();
+        var t_showbody=$div.find("."+cd.clazzName.NC_TS).first();
+        var t_hidebod=$div.find("."+cd.clazzName.NC_TH).first();
+        
+        if(!t_body&&(t_showbody&&t_hidebody)){
+            customBody=true;
+            $showbody=t_showbody
+            $hidebody=t_hidebod
+        }else{
+            customBody=false;
+            $showbody=$hidebody=t_body;
+        }
+    }
+}
 /**
  * Shows and hides content and picture (if available) of navigation bars.
  *
@@ -41,6 +201,7 @@ function toggleNavigationBar( indexNavigationBar, e ) {
                 frame.find( '> .NavPic, > .NavContent, > .toogleShow' ).each( function() {
                         $( this ).css( 'display', 'block' );
                 });
+                
                 frame.find( '> .toggleHide' ).each( function() {
                         $( this ).css( 'display', 'none' );
                 });
